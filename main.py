ce3d261937e1a0094deb79e46bfb09a1d0af625f
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-07-08 11:12:05 +0530

    bot main 🛡

---
+++ main.py
@@ -0,0 +1,18 @@
+from pyrogram.raw import functions, types
+from pyrogram import Client, idle
+from pyromod import listen
+from config import Config
+
+bot = Client(
+    "bot",
+    bot_token=Config.BOT_TOKEN,
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    workers=50,
+    plugins=dict(root="plugins")
+    )
+
+bot.start()
+
+idle()
+bot.stop()
